ac8fb3f3ac8ebc4add7577ab1c0fe6c4866ed338
Rahkill <rahkill@gmail.com>
2018-12-03 23:11:57 -0200

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -1,5 +1,6 @@
 const { Client, Util } = require('discord.js');
-const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
+const TOKEN = process.env.TOKEN;
+const PREFIX = process.env.PREFIX;
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
